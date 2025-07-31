c16f22c0008291511f0c51bc76acc2fe612d5d8b
Morgan <morganferris@hotmail.ca>
2025-07-31 12:27:24 -0600

    Update problem_1_dual_sum.py

--- assessment_submission/problem_1_dual_sum.py
+++ assessment_submission/problem_1_dual_sum.py
@@ -1,1 +1,9 @@
+def dual_sum(energies, key):
+    return sum(e for e in energies if e % key == 0)
+
+# Example Tests
+print(dual_sum([3, 6, 9, 12], 3))     # 30
+print(dual_sum([4, 8, 13, 17], 2))    # 12
+print(dual_sum([5, 10, 15], 4))       # 0
+print(dual_sum([], 3))               # 0
 
